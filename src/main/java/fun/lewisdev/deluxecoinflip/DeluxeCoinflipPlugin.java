3930cc2f1b9b3db2686bb307cf6f347bb278ed90
Save <itzsaveyt@gmail.com>
2023-10-04 14:55:59 -0500

    Update copyright year.

--- src/main/java/fun/lewisdev/deluxecoinflip/DeluxeCoinflipPlugin.java
+++ src/main/java/fun/lewisdev/deluxecoinflip/DeluxeCoinflipPlugin.java
@@ -47,7 +47,7 @@ public class DeluxeCoinflipPlugin extends JavaPlugin implements DeluxeCoinflipAP
         getLogger().log(Level.INFO, "");
         getLogger().log(Level.INFO, " __ __    DeluxeCoinflip v" + getDescription().getVersion());
         getLogger().log(Level.INFO, "/  |_     Author: " + getDescription().getAuthors().get(0));
-        getLogger().log(Level.INFO, "\\_ |      (c) Lewis D 2020-2022. All rights reserved.");
+        getLogger().log(Level.INFO, "\\_ |      (c) Lewis D 2020-2023. All rights reserved.");
         getLogger().log(Level.INFO, "");
 
         listenerCache = CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS).maximumSize(500).build();
