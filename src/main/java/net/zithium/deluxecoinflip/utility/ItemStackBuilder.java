cfa7b4d93f0deaf8dee62ce208438333ce4de8ea
Austin <itzsaveyt@gmail.com>
2023-12-11 14:38:14 -0600

    Remove deprecated/unused methods.

--- src/main/java/net/zithium/deluxecoinflip/utility/ItemStackBuilder.java
+++ src/main/java/net/zithium/deluxecoinflip/utility/ItemStackBuilder.java
@@ -6,15 +6,12 @@
 package net.zithium.deluxecoinflip.utility;
 
 import net.zithium.deluxecoinflip.utility.universal.XMaterial;
-import org.bukkit.Color;
-import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.ArrayList;
@@ -80,11 +77,6 @@ public class ItemStackBuilder {
         return builder;
     }
 
-    public ItemStackBuilder setType(Material material) {
-        ITEM_STACK.setType(material);
-        return this;
-    }
-
     public ItemStackBuilder withAmount(int amount) {
         ITEM_STACK.setAmount(amount);
         return this;
@@ -135,65 +127,12 @@ public class ItemStackBuilder {
         return this;
     }
 
-    @SuppressWarnings("deprecation")
-    public ItemStackBuilder withDurability(int durability) {
-        ITEM_STACK.setDurability((short) durability);
-        return this;
-    }
-
-    @SuppressWarnings("deprecation")
-    public ItemStackBuilder withData(int data) {
-        ITEM_STACK.setDurability((short) data);
-        return this;
-    }
-
-    public ItemStackBuilder withEnchantment(Enchantment enchantment, final int level) {
-        ITEM_STACK.addUnsafeEnchantment(enchantment, level);
-        return this;
-    }
-
-    public ItemStackBuilder withEnchantment(Enchantment enchantment) {
-        ITEM_STACK.addUnsafeEnchantment(enchantment, 1);
-        return this;
-    }
-
     public ItemStackBuilder withGlow() {
         final ItemMeta meta = ITEM_STACK.getItemMeta();
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         ITEM_STACK.setItemMeta(meta);
         ITEM_STACK.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 1);
         return this;
-    }
-
-    public ItemStackBuilder withType(Material material) {
-        ITEM_STACK.setType(material);
-        return this;
-    }
-
-    public ItemStackBuilder clearLore() {
-        final ItemMeta meta = ITEM_STACK.getItemMeta();
-        meta.setLore(new ArrayList<String>());
-        ITEM_STACK.setItemMeta(meta);
-        return this;
-    }
-
-    public ItemStackBuilder clearEnchantments() {
-        for (Enchantment enchantment : ITEM_STACK.getEnchantments().keySet()) {
-            ITEM_STACK.removeEnchantment(enchantment);
-        }
-        return this;
-    }
-
-    public ItemStackBuilder withColor(Color color) {
-        Material type = ITEM_STACK.getType();
-        if (type == Material.LEATHER_BOOTS || type == Material.LEATHER_CHESTPLATE || type == Material.LEATHER_HELMET || type == Material.LEATHER_LEGGINGS) {
-            LeatherArmorMeta meta = (LeatherArmorMeta) ITEM_STACK.getItemMeta();
-            meta.setColor(color);
-            ITEM_STACK.setItemMeta(meta);
-            return this;
-        } else {
-            throw new IllegalArgumentException("withColor is only applicable for leather armor!");
-        }
     }
 
     public ItemStack build() {
