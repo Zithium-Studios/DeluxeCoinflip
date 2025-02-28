2b19931504eea00564b64e3226042db36cc02872
Austin <itzsaveyt@gmail.com>
2025-02-27 18:42:01 -0600

    fix: add deprecation suppression to PlaceholderAPIHook

--- src/main/java/net/zithium/deluxecoinflip/hook/PlaceholderAPIHook.java
+++ src/main/java/net/zithium/deluxecoinflip/hook/PlaceholderAPIHook.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
+@SuppressWarnings("deprecation")
 public class PlaceholderAPIHook extends PlaceholderExpansion {
 
     private final DeluxeCoinflipPlugin plugin;
