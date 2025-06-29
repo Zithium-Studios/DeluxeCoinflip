405c9bacb5155989b40fc64460dc5f662c3088d7
danny <danny@atropos.cc>
2025-06-28 23:35:12 -0400

    fix: optimize imports

--- src/main/java/net/zithium/deluxecoinflip/storage/handler/impl/SQLiteHandler.java
+++ src/main/java/net/zithium/deluxecoinflip/storage/handler/impl/SQLiteHandler.java
@@ -19,7 +19,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
